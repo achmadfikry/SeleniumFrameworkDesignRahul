cd07b4180fe23dbf3cd9a3100659f1269fd155cc
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-07-23 15:24:38 +0700

    Create peoduct error validation

--- src/test/java/rahulshettyacademy/tests/ErrorValidations.java
+++ src/test/java/rahulshettyacademy/tests/ErrorValidations.java
@@ -1,22 +1,39 @@
 package rahulshettyacademy.tests;
 
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import rahulshettyacademy.TestComponents.BaseTest;
+import rahulshettyacademy.pageobjects.CartPage;
+import rahulshettyacademy.pageobjects.CheckoutPage;
+import rahulshettyacademy.pageobjects.ConfirmationPage;
 import rahulshettyacademy.pageobjects.ProductCatalogue;
 
 import java.io.IOException;
+import java.util.List;
 
 public class ErrorValidations extends BaseTest {
     @Test
-    public void SubmitOrderTest() throws InterruptedException, IOException {
+    public void LoginErrorValidation() throws InterruptedException, IOException {
         String productName = "ZARA COAT 3";
 
         ProductCatalogue productCatalogue = landingPage.loginApplication("rahulshe@yopmail.com", "Password123!");
 
         Assert.assertEquals("Incorrect email or password.",landingPage.getErrorMessage());
-
-
-
     }
+
+    @Test
+    public void ProductErrorValidation() throws InterruptedException, IOException {
+        String productName = "ZARA COAT 3";
+        ProductCatalogue productCatalogue = landingPage.loginApplication("rahulshetty123@yopmail.com", "Password123!");
+        List<WebElement> products = productCatalogue.getProductList();
+        productCatalogue.addProductToCart(productName);
+        CartPage cartPage = productCatalogue.goToCartPage();
+
+        //css selector: .cartSection h3 -> parent to child
+        //xpath: //*[@class='cartSection']/h3 -> parent to child
+        Boolean match = cartPage.VerifyProductDisplay("ZARA COAT 33");
+        Assert.assertFalse(match);
+    }
+
 }
