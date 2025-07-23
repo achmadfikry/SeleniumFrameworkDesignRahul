4ee6a7833f724b02ed3c67bd6b83c48ae669e838
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-07-23 11:08:13 +0700

    Condition if runnint on different browser

--- src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
+++ src/test/java/rahulshettyacademy/TestComponents/BaseTest.java
@@ -3,6 +3,8 @@ package rahulshettyacademy.TestComponents;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -11,15 +13,24 @@ import java.time.Duration;
 import java.util.Properties;
 
 public class BaseTest {
+    WebDriver driver;
     public void initializeDriver() throws IOException {
-
         //properties class
         Properties prop = new Properties();
         FileInputStream fis = new FileInputStream("/Users/telkomdev/Java/SeleniumFrameworkDesignRahul/src/main/java/rahulshettyacademy/resources/GlobalData.properties");
         prop.load(fis);
+        String browserName = prop.getProperty("browser");
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        if(browserName.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }else if(browserName.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }else if(browserName.equalsIgnoreCase("edge")) {
+            System.setProperty("webdriver.edge.driver", "edge.exe");
+            driver = new EdgeDriver();
+        }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
 
