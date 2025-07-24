864268858ad7f91f7e6039d30fd5dee0fd43c02a
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-07-24 14:14:34 +0700

    Immplementing parameterization using hashmap with TestNG Data Provider

--- src/test/java/rahulshettyacademy/tests/SubmitOrderTest.java
+++ src/test/java/rahulshettyacademy/tests/SubmitOrderTest.java
@@ -8,6 +8,7 @@ import rahulshettyacademy.TestComponents.BaseTest;
 import rahulshettyacademy.pageobjects.*;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 public class SubmitOrderTest extends BaseTest {
@@ -15,19 +16,20 @@ public class SubmitOrderTest extends BaseTest {
     String productName = "ZARA COAT 3";
 
     @Test(dataProvider = "getData", groups = {"Purchase","TestMoreThan1Group"})
-    public void SubmitOrderTest(String email, String password, String productName) throws InterruptedException, IOException {
-        ProductCatalogue productCatalogue = landingPage.loginApplication(email, password);
+//    public void SubmitOrderTest(String email, String password, String productName) throws InterruptedException, IOException {
+    public void SubmitOrderTest(HashMap<String,String> input) throws InterruptedException, IOException {
+        ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("email"), input.get("password"));
 
         List<WebElement> products = productCatalogue.getProductList();
 
-        productCatalogue.addProductToCart(productName);
+        productCatalogue.addProductToCart(input.get("product"));
 
         CartPage cartPage = productCatalogue.goToCartPage();
 
         //css selector: .cartSection h3 -> parent to child
         //xpath: //*[@class='cartSection']/h3 -> parent to child
 
-        Boolean match = cartPage.VerifyProductDisplay(productName);
+        Boolean match = cartPage.VerifyProductDisplay(input.get("product"));
         Assert.assertTrue(match);
 
         CheckoutPage checkoutPage = cartPage.goToCheckout();
@@ -48,6 +50,17 @@ public class SubmitOrderTest extends BaseTest {
 
     @DataProvider
     public Object[][] getData(){
-        return new Object[][] {{"rahulshetty123@yopmail.com", "Password123!", "ZARA COAT 3"},{"shetty@gmail.com", "Iamking@000", "ADIDAS ORIGINAL"}};
+//        return new Object[][] {{"rahulshetty123@yopmail.com", "Password123!", "ZARA COAT 3"},{"shetty@gmail.com", "Iamking@000", "ADIDAS ORIGINAL"}};
+        HashMap<String,String> map = new HashMap<String,String>();
+        map.put("email","rahulshetty123@yopmail.com");
+        map.put("password","Password123!");
+        map.put("product","ZARA COAT 3");
+
+        HashMap<String,String> map1 = new HashMap<String,String>();
+        map1.put("email","rahulshetty12@yopmail.com");
+        map1.put("password","Password123!");
+        map1.put("product","ADIDAS ORIGINAL");
+
+        return new Object[][]{{map},{map1}};
     }
 }
