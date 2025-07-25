43c1eb82031ada7b753d862efb09a2d4e40b3bfa
telkomdev <telkomdev@telkomdevs-MacBook-Pro.local>
2025-07-25 11:29:12 +0700

    Initiate Extent Report

---
+++ src/test/java/rahulshettyacademy/TestComponents/Listeners.java
@@ -0,0 +1,65 @@
+package rahulshettyacademy.TestComponents;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import rahulshettyacademy.resources.ExtentReporterNG;
+
+public class Listeners implements ITestListener {
+
+    ExtentTest test;
+    ExtentReports extent = ExtentReporterNG.getReportObject();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        test = extent.createTest(result.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        test.log(Status.PASS, "Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        //1. notif failed
+        test.fail(result.getThrowable());
+
+        //2. screenshot
+
+        //3. attach to the report
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        ITestListener.super.onTestSkipped(result);
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        ITestListener.super.onTestFailedButWithinSuccessPercentage(result);
+    }
+
+    @Override
+    public void onTestFailedWithTimeout(ITestResult result) {
+        ITestListener.super.onTestFailedWithTimeout(result);
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        ITestListener.super.onStart(context);
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        ITestListener.super.onFinish(context);
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return ITestListener.super.isEnabled();
+    }
+}
